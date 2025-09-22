333adc97eb1e67132d79093488e8f559fa78aeb5
mitayan0 <mitayanchangma@gmail.com>
2025-09-22 22:06:24 +0600

    setup file update

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-import subprocess
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
 
 setup(
-  name='multicorn_fdw',
-  version='0.0.1',
-  author='balabla',
-  license='Postgresql',
-  packages=['multicorn_fdw']
+    name='multicorn_fdw',
+    version='0.0.1',
+    author='Mitayan Chakma',
+    description='A PostgreSQL Foreign Data Wrapper built with Multicorn.',
+    license='Postgresql',
+    packages=find_packages(),
+    install_requires=[
+        'multicorn'
+    ]
 )
